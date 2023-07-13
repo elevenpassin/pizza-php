655dcf1af5a379765c86bd93af2bbb4216b63ff8
buoyantair <buoyantair@protonmail.com>
2023-07-13 20:38:06 +0200

    add delete functionality

--- details.php
+++ details.php
@@ -1,6 +1,19 @@
 <?php 
   include('./config/db_connect.php');
 
+  // handle request to delete pizza
+  if (isset($_POST['delete'])) {
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if(mysqli_query($conn, $sql)) {
+      header('Location: index.php');      
+    } else {
+      echo 'query error: ' . mysqli_error($conn);
+    }
+    
+  }
+
   // check get request "id" parameter
   if (isset($_GET['id'])) {
     $id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -40,6 +53,12 @@
   <p>Created by: <?php echo htmlspecialchars($pizza['email']) ?></p>
   <p>Created at: <?php echo date($pizza['created_at']) ?></p>
   <?php include('./template-parts/ingredients.php'); ?>
+
+  <form action="./details.php" method="post">
+    <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+    <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+  </form>
+
   <?php else: ?>
   <p>No problem! Create your own pizza down here...!</p>
   <a href="add.php" class="btn btn-primary">Create my own pizza</a>
